9697620cb6ae08c645ab56ad1182ea90d32b810d
aloseun137 <oluwaseunoffice@gmail.com>
2022-02-23 00:13:45 +0100

    .htaccess changes

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** Database settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define( 'DB_NAME', 'newtrjwebsite' );
+define( 'DB_NAME', 'wordpressdb' );
 
 /** Database username */
 define( 'DB_USER', 'root' );
 
 /** Database password */
-define( 'DB_PASSWORD', 'trj2021passwordlimited!!' );
+define( 'DB_PASSWORD', '' );
 
 /** Database hostname */
 define( 'DB_HOST', 'localhost' );
